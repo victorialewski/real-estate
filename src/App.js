3942662668354352154aa075954213ad842ce8d1
victoria chmielewski <plfouch@gmail.com>
2025-05-06 09:20:24 -0500

    version update for deployment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import './App.css';
 
-import{BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-import Navbar from './components/NavBar/Navbar';
-import SowLeft from './components/Calculations/SowLeft';
+import{HashRouter as Router, Routes, Route} from 'react-router-dom';
 import SowPage from './components/SowPage';
-// import Investment from './components/ARV/Investment';
-// import ArvCalculator from './components/ARV/Arv';
-// import Rehab from './components/Rehab/Rehab';
-
-// import ContactPage from './components/Contact/ContactPage';
 
 function App() {
   return (
     <div className="App">
-
-{/* <Navbar></Navbar> */}
 <Router>
     <Routes>
-
-      <Route path='/' element={<SowPage/>}/>
-      <Route path='/test' element={<SowLeft/>} />
-      {/* <Route path='/arv' element={<ArvCalculator/>} />
-      <Route path='/rehab' element={<Rehab/>} /> */}
-
-
-
+    <Route path='https://victorialewski.github.io/real-estate/' element={<SowPage />} />
     </Routes>
      </Router>
-
-
 
     </div>
     
